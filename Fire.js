a390d41a2450c000f247108427214398566a70a3
docaotri123 <docaotri211997@gmail.com>
2018-11-30 00:00:27 +0700

    init

---
+++ Fire.js
@@ -0,0 +1,83 @@
+import firebase from 'firebase';
+
+class Fire {
+    constructor() {
+        this.init();
+        this.observeAuth();
+    }
+
+    init = () =>
+        firebase.initializeApp({
+            apiKey: "AIzaSyA-LUx5bYnJ_0VZ5pw-p7zxGc2nUrazX20",
+            authDomain: "chatapp-c3804.firebaseapp.com",
+            databaseURL: "https://chatapp-c3804.firebaseio.com",
+            projectId: "chatapp-c3804",
+            storageBucket: "chatapp-c3804.appspot.com",
+            messagingSenderId: "660668018593"
+        });
+
+    observeAuth = () =>
+        firebase.auth().onAuthStateChanged(this.onAuthStateChanged);
+
+    onAuthStateChanged = user => {
+        if (!user) {
+            try {
+                firebase.auth().signInAnonymously();
+            } catch ({ message }) {
+                alert(message);
+            }
+        }
+    };
+
+    get uid() {
+        return (firebase.auth().currentUser || {}).uid;
+    }
+
+    get ref() {
+        return firebase.database().ref('messages');
+    }
+
+    parse = snapshot => {
+        const { timestamp: numberStamp, text, user } = snapshot.val();
+        const { key: _id } = snapshot;
+        const timestamp = new Date(numberStamp);
+        const message = {
+            _id,
+            timestamp,
+            text,
+            user,
+        };
+        return message;
+    };
+
+    on = callback =>
+        this.ref
+            .limitToLast(20)
+            .on('child_added', snapshot => callback(this.parse(snapshot)));
+
+    get timestamp() {
+        return firebase.database.ServerValue.TIMESTAMP;
+    }
+    // send the message to the Backend
+    send = messages => {
+        for (let i = 0; i < messages.length; i++) {
+            const { text, user } = messages[i];
+            const message = {
+                text,
+                user,
+                timestamp: this.timestamp,
+            };
+            this.append(message);
+        }
+    };
+
+    append = message => this.ref.push(message);
+
+    // close the connection to the Backend
+    off() {
+        this.ref.off();
+    }
+}
+
+Fire.shared = new Fire();
+export default Fire;
---
+++ components/Main.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+class Main extends React.Component {
+  static navigationOptions = {
+    title: 'Chat App',
+  };
+
+  state = {
+    name: '',
+  };
+
+  onPress = () =>
+    this.props.navigation.navigate('Chat', { name: this.state.name });
+
+  onChangeText = name => this.setState({ name });
+
+  render() {
+    return (
+      <View>
+        <Text style={styles.title}>Enter your name:</Text>
+        <TextInput
+          style={styles.nameInput}
+          placeHolder="Your name"
+          onChangeText={this.onChangeText}
+          value={this.state.name}
+        />
+        <TouchableOpacity onPress={this.onPress}>
+          <View>
+            <Text style={styles.buttonText}>Next</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const offset = 24;
+const styles = StyleSheet.create({
+  title: {
+    marginTop: offset,
+    marginLeft: offset,
+    fontSize: offset,
+  },
+  nameInput: {
+    height: offset * 2,
+
+    margin: offset,
+    paddingHorizontal: offset,
+    borderColor: '#111111',
+    borderWidth: 1,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#2196F3'
+  },
+  buttonText: {
+    marginLeft: offset,
+    fontSize: offset,
+  },
+});
+
+export default Main;
